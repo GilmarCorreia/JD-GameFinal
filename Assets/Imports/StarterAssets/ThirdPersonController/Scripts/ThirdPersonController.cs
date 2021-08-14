72e0fad27f64b5aa0e25fe7ab0237d14e55a2dfb
lucas <lucasset10@msn.com>
2021-08-14 20:19:10 -0300

    Prando ao pegar objeto

--- Assets/Imports/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/Imports/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -125,6 +125,7 @@ namespace StarterAssets
 		//bool lastShootInput = false;
 		//bool pullingString = false;
 		public bool testAim = false;
+		public bool pickingObject = false;
 
 		[Header("Aim Camera Settings")]
 		public float cameraYrotation = 0;
@@ -206,10 +207,16 @@ namespace StarterAssets
 
 		private void PickObject()
         {
-            if (_input.pickObject && this._canPickItem)
-            {
+			if (_input.pickObject && this._canPickItem)
+			{
 				_animator.SetBool(_animIDPickBow, true);
+				pickingObject = true;
 			}
+		}
+
+		public void FinishPickingItem()
+        {
+			pickingObject = false;
 		}
    
         private void ShootingMecanics()
@@ -342,7 +349,7 @@ namespace StarterAssets
 
 			// note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
 			// if there is no input, set the target speed to 0
-			if (_input.move == Vector2.zero) targetSpeed = 0.0f;
+			if (_input.move == Vector2.zero || pickingObject) targetSpeed = 0.0f;
 
 			// a reference to the players current horizontal velocity
 			float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
